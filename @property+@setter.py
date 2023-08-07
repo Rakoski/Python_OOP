3d474c5522ac37f6afecbe58c0b934143d6f55eb
Rakoski <mastrakoski@gmail.com>
2023-08-07 03:46:54 -0300

    Getters and setters in Python!

--- @property+@setter.py
+++ @property+@setter.py
@@ -1,20 +1,34 @@
+# Getters e Setters em Python!
+
 class Caneta:
     def __init__(self, cor):
         self.cor_tinta = cor
+        self._cor_tampa = None
 
         # olha, não usem esse atributo!
         # atributos que começam em _ ou __ não devem ser usados fora da classe
         self._cor = self.cor_tinta
 
-
+    # Já esse aqui seria o "getter" do python
     @property
     def cor(self):
-        print('property')
-        return 'Qualquer'
+        print("estou no getter")
+        return self._cor
 
+    # setters no python se faz assim
     @cor.setter
     def cor(self, valor):
+        if valor == "Rosa":
+            raise ValueError("Não aceito essa cor")
         print("Estou no setter", valor)
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
 
 
 def mostrar(caneta):
@@ -22,5 +36,8 @@ def mostrar(caneta):
 
 
 caneta = Caneta('Azul')
-caneta.cor = 'Rosa'
+caneta.cor = "Pink"
+print(caneta.cor_tampa)
+caneta.cor_tampa = "Azul"
+print(caneta.cor_tampa)
 print(caneta.cor)
